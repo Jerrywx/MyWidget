15a49d63833649a0335499302526c3160722ad2c
wangxiao <wangxiao117210@zongheng.com>
2020-11-03 17:38:33 +0800

    添加项目

---
+++ MyWidget/MyWidget/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MyWidget
+//
+//  Created by 王潇 on 2020/11/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
