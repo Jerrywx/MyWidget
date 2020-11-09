be298046d041eb2c30b5640342d82f19b1e5db3d
Jerrywx <23601145@qq.com>
2020-11-09 21:14:00 +0800

    no message

---
+++ MyWidget/PokeMaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeMaster
+//
+//  Created by J on 2020/11/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyWidget/PokeMaster/PokeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeMasterApp.swift
+//  PokeMaster
+//
+//  Created by J on 2020/11/9.
+//
+
+import SwiftUI
+
+@main
+struct PokeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
